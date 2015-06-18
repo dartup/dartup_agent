56053bca368d31a1caf728deb445aec8bf955ac8
Ole Martin Gjersvik <olem@olem.org>
2015-06-18 16:34:27 +0200

    Added base scaffolding for the program.

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,40 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
 
-main(List<String> arguments) {
-  print('Hello world');
+main() async {
+  List sites = await assignNames(getFromDB()).toList();
+
+  await startNgnix(writeNgnixConf(sites));
+
+  await Future.wait(sites.map(runSite));
 }
+
+Future runSite(Site site) => addUser(site).then(cloneGit).then(pubGet).then(startServer);
+
+class Site{}
+
+/// @todo add meat.
+Stream<Site> getFromDB(){}
+
+/// @todo add meat.
+Stream<Site> assignNames(Stream<Site> sites){}
+
+/// @todo add meat.
+String writeNgnixConf(List<Site>){}
+
+/// @todo add meat.
+Future startNgnix(String conf){}
+
+/// @todo add meat.
+Future<Site> addUser(Site site){}
+
+/// @todo add meat.
+Future<Site> cloneGit(Site site){}
+
+/// @todo add meat.
+Future<Site> pubGet(Site site){}
+
+/// @todo add meat.
+Future<Site> startServer(Site site){}
