e29ea861401cdbe12631d255891e7ee9a8839a75
Ole Martin Gjersvik <olem@olem.org>
2015-06-18 16:53:15 +0200

    Made fake implementations off all functions.

--- bin/main.dart
+++ bin/main.dart
@@ -13,28 +13,75 @@ main() async {
 
 Future runSite(Site site) => addUser(site).then(cloneGit).then(pubGet).then(startServer);
 
-class Site{}
+class Site{
+  String name = '';
+  String gitUrl = '';
+  Map<String,String> envVars = {};
+  String user = 'nobody';
+  int port = 0;
+}
 
 /// @todo add meat.
-Stream<Site> getFromDB(){}
+Stream<Site> getFromDB(){
+  var fakeData = [];
+  fakeData.add(new Site()
+    ..name = 'fake1'
+    ..gitUrl = 'git://exsample.com/'
+  );
+  fakeData.add(new Site()
+    ..name = 'fake2'
+    ..gitUrl = 'git://exsample.com/'
+  );
+  fakeData.add(new Site()
+    ..name = 'fake3'
+    ..gitUrl = 'git://exsample.com/'
+  );
+
+  return new Stream.fromIterable(fakeData);
+}
 
 /// @todo add meat.
-Stream<Site> assignNames(Stream<Site> sites){}
+Stream<Site> assignNames(Stream<Site> sites){
+  var i = 1;
+  return sites.map((s){
+    s.user = 'user$i';
+    s.port = 8000 + i;
+    i += 1;
+    return s;
+  });
+}
 
 /// @todo add meat.
-String writeNgnixConf(List<Site>){}
+String writeNgnixConf(List<Site> sites){
+  return sites.map((s) => '${s.name}.dartup.io').join('\n');
+}
 
 /// @todo add meat.
-Future startNgnix(String conf){}
+Future startNgnix(String conf) async{
+  print('Fake Ngnix started');
+  print(conf);
+}
 
 /// @todo add meat.
-Future<Site> addUser(Site site){}
+Future<Site> addUser(Site site) async{
+  print('Fake adder user: ${site.user}');
+  return site;
+}
 
 /// @todo add meat.
-Future<Site> cloneGit(Site site){}
+Future<Site> cloneGit(Site site)async{
+  print('Fake clone git: ${site.gitUrl}');
+  return site;
+}
 
 /// @todo add meat.
-Future<Site> pubGet(Site site){}
+Future<Site> pubGet(Site site)async{
+  print('Fake pubGet');
+  return site;
+}
 
 /// @todo add meat.
-Future<Site> startServer(Site site){}
+Future<Site> startServer(Site site)async{
+  print('Fake started ${site.name}');
+  return site;
+}
